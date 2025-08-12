691a06ca04b6d4b40d2f902c62906f7e37a7bb14
jfoj-dev <jfoj365@gmail.com>
2025-08-11 23:31:11 -0300

    Criando métricas na tela Dashboard

--- app/urls.py
+++ app/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
+    path('', views.home, name='home'),
+
     path('', include('brands.urls')),
     path('', include('categories.urls')),
     path('', include('suppliers.urls')),
