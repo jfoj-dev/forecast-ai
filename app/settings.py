f16796beb0c7650caf79df790d8506d843d40d6c
jfoj-dev <jfoj365@gmail.com>
2025-09-25 23:10:07 -0300

    Primeira Carga dos Dados

--- app/settings.py
+++ app/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'estoque',
+        'USER': 'postgres',
+        'PASSWORD': '2qmzzx5@',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
