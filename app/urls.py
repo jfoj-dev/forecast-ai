122aadff7430110556d96571b22d585ef73666a3
jfoj-dev <jfoj365@gmail.com>
2025-10-18 20:25:14 -0300

    Finalizando tela de login, ajustes de usuarios, iniciando desenvolvimento dos grupos de usuarios.

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
     # Dashboard / página inicial
     path('', views.home, name='home'),
